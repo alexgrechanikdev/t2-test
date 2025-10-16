a04c655bc608f3aa39e2d5ba3e730d20c7f8d75e
Aleksandr Grechanik <Aleksandr.Grechanik@everise.com>
2025-10-16 23:17:29 +0300

    change get data

--- src/api/api.tsx
+++ src/api/api.tsx
@@ -1,7 +1,15 @@
+import { PRODUCT_DATA } from "../constants";
 import type { IProduct } from "../types";
 
+async function getArrayAsync(): Promise<Array<IProduct>> {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(PRODUCT_DATA);
+    }, 1000);
+  });
+}
+
 export const fetchData = async (): Promise<Array<IProduct>> => {
-  const res = await fetch("../../data.json");
-  const { data } = await res.json();
-  return data;
+  const res = await getArrayAsync();
+  return res;
 };
